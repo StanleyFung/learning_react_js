25abed612662bf9197bc8b4bce92d06f3ad776d4
StanleyFung <stanleyfung94@hotmail.com>
2017-10-24 15:35:05 -0400

    client

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -1,19 +1,40 @@
-import {List, Map} from 'immutable';
+import { List, Map } from 'immutable';
+
+export const INITIAL_STATE = Map();
 
 export function setEntries(state, entries) {
-  return state.set('entries', List(entries));
+    return state.set('entries', List(entries));
+}
+
+function getWinners(vote) {
+    if (!vote) return [];
+    const [a, b] = vote.get('pair');
+    const aVotes = vote.getIn(['tally', a], 0)
+    const bVotes = vote.getIn(['tally', b], 0)
+
+    if (aVotes > bVotes) return [a];
+    else if (bVotes > aVotes) return [b];
+    else return [a, b];
 }
 
 export function next(state) {
-    const entries = state.get('entries');
-    return state.merge({
-        vote: Map({pair: entries.take(2)}),
-        entries: entries.skip(2)
-    });
+    const votes = state.get('vote')
+    const winners = getWinners(votes);
+    const entries = state.get('entries').concat(winners);
+
+    if (entries.size === 1) {
+        return state.remove('vote').remove('entries')
+            .set('winner', entries.first());
+    } else {
+        return state.merge({
+            vote: Map({ pair: entries.take(2) }),
+            entries: entries.skip(2)
+        });
+    }
 }
 
-export function vote(state, entry) {
-    return state.updateIn(
-        ['vote', 'tally'], 0, tally => tally + 1
+export function vote(voterState, entry) {
+    return voterState.updateIn(
+        ['tally', entry], 0, tally => tally + 1
     )
 }
